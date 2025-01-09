1e508c5c8a6b6feeec9a9489678f67b34803be22
FranPerez <vricheb973@g.educaand.es>
2025-01-09 13:07:34 +0100

    Actualizados apuntes

---
+++ ApuntesTema6/src/ejemplo1/Main3.java
@@ -0,0 +1,17 @@
+package ejemplo1;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+
+		Persona p1 = new Persona("vladi", 32, 2.0);
+		Persona p2 = new Persona("pepe", 35, 1.98);	
+		
+		p1.presentarse();
+		p2.presentarse();
+		
+		
+
+	}
+
+}
--- ApuntesTema6/src/ejemplo1/Persona.java
+++ ApuntesTema6/src/ejemplo1/Persona.java
@@ -3,11 +3,38 @@ package ejemplo1;
 public class Persona {
 	
 	//Atributos
+	int id;
 	String nombre;
 	int edad;
 	double estatura;
+
+	//Atributos estáticos
+	static int contadorId = 1;
+	static String nombreUltimaPersona;
 	
 	//Constructores
+	public Persona() {
+		this.id = Persona.contadorId++;
+	}
+	
+	public Persona(String nombre, int edad, double estatura){
+		if(nombre.isBlank() || nombre == null) {
+			throw new IllegalArgumentException("El nombre no puede ser vacío, ni en blanco, ni nulo");
+		}
+		if(edad < 18) {
+			throw new IllegalArgumentException("La edad debe ser mayor o igual a 18 años");
+		}
+		if(estatura < 1.0) {
+			throw new IllegalArgumentException("La estatura debe ser mayor o igual que 1 metro.");
+		}
+		
+		this.id = Persona.contadorId++;		
+		this.nombre = nombre;
+		this.edad = edad;
+		this.estatura = estatura;
+		
+		Persona.nombreUltimaPersona = nombre;
+	}
 	
 	//Métodos
 	void saludar() {
@@ -27,10 +54,23 @@ public class Persona {
 	}
 	
 	void presentarse() {
+		System.out.println("Mi ID es " + this.id);
 		System.out.println("Mi nombre es " + this.nombre);
 		System.out.println("Tengo " + this.edad + " años");
 		System.out.println("Mido " + this.estatura + "m");
+		System.out.println("El contador está " + Persona.contadorId);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
